7e1003b0081daf2b8ba20beaf24b006ef7bd7d78
Orsouene <orsoueneelaouizeb@gmail.com>
2024-12-02 16:14:05 +0100

    Creato un array dove ho inserito una lista di 5 post

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 //  rotta che ritorna un testo
-app.get("/", (req, res) => {
+app.get("/dolce", (req, res) => {
   res.send("Server del mio blog");
 });
 app.listen(PORT, () => {
   console.log(`il mio server http://localhost:${PORT}`);
 });
+
+// file statici
+app.use(express.static("public"));
+// importare lista.js
+const lista = require("./lista");
